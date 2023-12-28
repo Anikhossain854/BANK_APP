2ead5905f9f718244ceb865850500e22fd068d31
Anikh <Anikh854@gmail.com>
2023-12-28 17:01:43 +0600

    new added

--- bank.js
+++ bank.js
@@ -14,48 +14,38 @@ button.addEventListener('click',function(){
     
     
 })
+//  deposit
+
 depositBtn.addEventListener('click',function(){
 
- const depositAmount = document.getElementById('depositAmount').value;
- const depositNumer = parseFloat(depositAmount);
-   
-//  deposit
-    const currentDeposit = document.getElementById('currentDeposit').innerText;
-    const currentNumner = parseFloat(currentDeposit);
-    const totalDeposit = depositNumer + currentNumner;
-     document.getElementById('currentDeposit').innerText = totalDeposit;
-  
-    //   TotalBalace
-    const totalBalace = document.getElementById('totalBalace').innerText;
-    const totalBalaceNumner = parseFloat(totalBalace);
-    const newBalace = depositNumer + totalBalaceNumner;
-    document.getElementById('totalBalace').innerText =newBalace;
-    
+   const depositAmount = document.getElementById('depositAmount').value;
+   const depositNumer = parseFloat(depositAmount);
 
-
-
-  document.getElementById('depositAmount').value='';
-
-
-
+ udpadateSpanText('currentDeposit',depositNumer);
+ udpadateSpanText('currentBalace',depositNumer);
+ document.getElementById('depositAmount').value='';
 
 });
-
-// withdraw
-
 withdrawBtn.addEventListener('click',function(){
-
- const withdraNumber = getInputNumber('withdrawAmout')
-
-
-})
-
+const withdrawNumber =getInputNumber('withdrawAmout');
+console.log(withdrawNumber)
+}) 
 
 function getInputNumber(id){
-    const withdrawAmout = document.getElementById(id).value;
-    const  withdraNumber  = parseFloat(withdrawAmout);
-    // console.log(withdraNumber)
-    return withdraNumber;
+    const amount = document.getElementById(id).value;
+    const amoutNumber = parseFloat(amount);
+    return amoutNumber;
+
+}
+
+
+
+function udpadateSpanText(id,depositNumer){
+    
+    const current = document.getElementById(id).innerText;
+    const currentNumner = parseFloat(current);
+    const totalAmount = depositNumer + currentNumner;
+    document.getElementById(id).innerText =totalAmount;
 
 }
 
